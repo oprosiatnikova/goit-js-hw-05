63d0beebceced791efd5beb10f9d1b6371bf138f
Olena Prosiatnikova <margulay1976@gmail.com>
2025-01-06 18:39:24 +0200

    corrections have been made

--- js/task-3.js
+++ js/task-3.js
@@ -4,8 +4,9 @@
 // Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.
 // У консоль будуть виведені результати її роботи.
 
-const sortByDescendingFriendCount = (users) =>
-    users.toSorted((a, b) => b.friends.length - a.friends.length);
+const sortByDescendingFriendCount = (users) => {
+  return users.toSorted((a, b) => b.friends.length - a.friends.length);
+};
 
 console.log(
   sortByDescendingFriendCount([
